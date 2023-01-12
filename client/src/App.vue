1d436061818338b6789bbb594d08b982243f4b40
slivcaigor <slivcaigor@gmail.com>
2023-01-12 15:55:20 +0100

    Files Structure

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import ToDo from "./components/ToDo.vue";
+
+export default {
+  components: {
+    ToDo,
+  },
+};
+</script>
+
+<template>
+  <ToDo />
+</template>
+
+<style lang="scss" scoped>
+@use "./styles/general.scss" as *;
+@use "./styles/partials/mixins" as *;
+@use "./styles/partials/variables" as *;
+</style>
---
+++ client/src/components/ToDo.vue
@@ -0,0 +1,12 @@
+<script></script>
+
+<template>
+  <h1>Ciao Mondo</h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  color: #888;
+  font-size: 100px;
+}
+</style>
